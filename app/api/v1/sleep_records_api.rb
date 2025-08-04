feed634cf81c766a393819b91eacc1760c54f831
Anthony Salim <tenebralis90@yahoo.com>
2025-08-04 13:17:26 +0700

    endpoint to clock out

--- app/api/v1/sleep_records_api.rb
+++ app/api/v1/sleep_records_api.rb
@@ -12,13 +12,37 @@ module V1
         user = UserClient.find(params[:user_id])
         present_error('User not found', code: 404) unless user
 
+        # If user has clocked in, display validation
+        record = SleepRecord.where(user_id: user['id'], clock_out: nil).order(created_at: :desc).first
+        present_error("User hasn't clocked out yet", code: 404) if record
+
         # Create the sleep record
         record = SleepRecord.create!(user_id: params[:user_id], clock_in: Time.current)
 
         present_success(record,'Clocked in successfully')
       end
 
-      desc 'Fetch all clock-ins'
+      desc 'Clock out (end sleeping)'
+      params do
+        requires :user_id, type: String, desc: 'ID of the user clocking in'
+      end
+      put ':user_id' do
+        # Validate user from user-service
+        user = UserClient.find(params[:user_id])
+        present_error('User not found', code: 404) unless user
+
+        # If user has clocked in, display validation
+        record = SleepRecord.where(user_id: user['id'], clock_out: nil).order(created_at: :desc).first
+        present_error('No active sleep session found', code: 422) unless record
+
+        record.update!(
+          clock_out: Time.current,
+          duration: (Time.current - record.clock_in).to_i
+        )
+        present_success(record,'Clocked out successfully')
+      end
+
+      desc 'Fetch all records'
       get do
         # Example stubbed return
         [
